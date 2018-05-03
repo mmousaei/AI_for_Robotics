549f4f608bd5bde0cc6208595e373b6b8124d280
mmousaei <m.mousaei@gmail.com>
2018-05-03 15:14:23 -0400

    Quiz_2 added

---
+++ Session1/Quiz2.py
@@ -0,0 +1,17 @@
+#Modify the program to find and print the sum of all 
+#the entries in the list p.
+
+#AI_for_Robotics
+#Quiz2
+#Mohammadreza Mousaei
+
+p = [0.2, 0.2, 0.2, 0.2, 0.2]
+pHit = 0.6
+pMiss = 0.2
+
+mask = [pMiss, pHit, pHit, pMiss, pMiss]
+
+p = [x*y for x,y in zip(mask, p)]
+
+
+print(sum(p))
