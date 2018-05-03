6363e2963522cd1c11832821a535eec32305706e
mmousaei <m.mousaei@gmail.com>
2018-05-03 18:51:59 -0400

    Quiz5 added

---
+++ Session1/Quiz5.py
@@ -0,0 +1,29 @@
+#The code so that it updates the probability twice
+#and gives the posterior distribution after both 
+#measurements are incorporated. Make sure that your code 
+#allows for any sequence of measurement of any length.
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+	q = []
+	for i in range(len(p)):
+		hit = (Z == world[i])
+		q.append(p[i]*(hit*pHit + (1-hit)*pMiss))
+
+	q = [x/sum(q) for x in (q)]
+	return q
+
+
+
+for i in range(len(measurements)):
+
+	p = sense(p, measurements[i])
+
+
+
+print(p)
