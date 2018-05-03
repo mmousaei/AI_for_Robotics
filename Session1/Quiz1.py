f27253814d5014025872b79afbc5a9ca338774c0
mmousaei <m.mousaei@gmail.com>
2018-05-03 15:08:39 -0400

    Quiz_1

---
+++ Session1/Quiz1.py
@@ -0,0 +1,18 @@
+#code that outputs p after multiplying each entry 
+#by pHit or pMiss at the appropriate places. Remember that
+#the red cells 1 and 2 are hits and the other green cells
+#are misses.
+
+#AI_for_Robotics
+#Quiz1
+#Mohammadreza Mousaei
+
+p = [0.2 0.2 0.2 0.2 0.2]
+pHit = 0.6
+pMiss = 0.2
+
+mask = [pMiss, pHit, pHit, pMiss, pMiss]
+
+p = [x*y for x,y in zip(mask, p)]
+
+print(p)
